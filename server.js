990747930fa3cbf4ee0e5713b86747b91e80e320
Vineet Pande <vsp4991@gmail.com>
2021-03-08 21:55:07 +0000

    working version

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // load .env data into process.env
 require('dotenv').config();
-
 // Web server config
 const PORT = process.env.PORT || 8080;
 const ENV = process.env.ENV || "development";
@@ -9,19 +8,8 @@ const bodyParser = require("body-parser");
 const sass = require("node-sass-middleware");
 const app = express();
 const morgan = require('morgan');
-
-// // PG database client/connection setup
-// const { Pool } = require('pg');
-// const dbParams = require('./lib/db.js');
-// const db = new Pool(dbParams);
-// db.connect();
-
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
-
 app.set("view engine", "ejs");
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/styles", sass({
   src: __dirname + "/styles",
@@ -30,36 +18,15 @@ app.use("/styles", sass({
   outputStyle: 'expanded'
 }));
 app.use(express.static("public"));
-
-// Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
-// const usersRoutes = require("./routes/users");
-// const widgetsRoutes = require("./routes/widgets");
-
-// Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
-// app.use("/api/users", usersRoutes(db));
-// app.use("/api/widgets", widgetsRoutes(db));
-// Note: mount other resources here, using the same pattern above
-
-
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
-// app.get("/", (req, res) => {
-//   res.render("index");
-// });
 const homeRoutes = require('./routes/home_routes.js');
 const voteRoutes = require('./routes/vote_routes.js');
 const pollRoutes = require('./routes/poll_routes.js');
 const resultRoutes = require('./routes/result_routes.js');
-
 app.use('/', homeRoutes);
 app.use('/vote/:poll_id', voteRoutes);
 app.use('/poll', pollRoutes);
 app.use('/poll/:poll_id', pollRoutes);
 app.use('results/:poll_id', resultRoutes);
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
