6e3d8d2f013f0821f2ee066289544c6abd4b30f3
Samantha Knoop <sknoop@hotmail.ca>
2021-03-08 00:57:43 +0000

    add semicolons to statements

--- db/queries/01_select_all_users.js
+++ db/queries/01_select_all_users.js
@@ -1,11 +1,11 @@
-const dbParams = require("../../lib/db")
+const dbParams = require("../../lib/db");
 
 const getAllUsers = () => {
   return dbParams.query('SELECT * FROM users;')
     .then((res) => {
-      console.log(res.rows)
+      console.log(res.rows);
       //return res.rows;
-    })
-}
+    });
+};
 
 module.exports = getAllUsers;
