e672e3e8bb1a66fa4bcd14a57a7a815f1d1be169
Samantha Knoop <sknoop@hotmail.ca>
2021-03-11 00:44:32 +0000

    fix spacing and add semi-colons for poll_routes

--- routes/poll_routes.js
+++ routes/poll_routes.js
@@ -1,54 +1,50 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const { insertNewPoll,
-  updateURLs, insertOptions } = require('../db/queries/insert_new_poll.js')
-
-
+const { insertNewPoll, updateURLs, insertOptions } = require('../db/queries/insert_new_poll.js');
 const { getUser } = require('../db/queries/user_queries.js');
+
 
 router.get('/:u_id', (req, res) => {
 
-  // let u_id = req.session.email;
-  // userObj = users[u_id];
-  // const urlsByUser = urlsForUser(u_id, urlDatabase);
-  // const templateVars = { urls: urlsByUser, user: userObj };
-  // res.render("index", templateVars);
- // console.log(req.params.u_id);
   let userObj;
   return getUser(req.params.u_id)
-  .then((resp) => {
-    //console.log("lplplplpl");
-    console.log(resp);
-    userObj = {resp};
-    const templateVars = {user : userObj};
-    res.render('create_poll', templateVars);
+    .then((resp) => {
+      console.log(resp);
+      userObj = {resp};
+      const templateVars = {user : userObj};
+      res.render('create_poll', templateVars);
 
-  })
-  .catch(er => console.log('ERROR',er));
-
+    })
+    .catch(er => console.log('ERROR',er));
 
 });
+
+
 //Renders admin_poll page where the user enters their options
 router.get('/:poll_id', (req, res) => {
 
   return updateURLs(req.params)
-  .then((results) => {
+    .then((results) => {
+      console.log(results);
+      const templateVars = {poll_id: req.params.poll_id, opNum : results[0].numops};
+      res.render('admin_poll', templateVars);
 
-    console.log(results);
-    const templateVars = {poll_id: req.params.poll_id, opNum : results[0].numops}
-    res.render('admin_poll', templateVars)
+    })
+    .catch(er => console.log('ERROR',er));
 
-  })
-  .catch(er => console.log('ERROR',er))
 });
+
+
 //User enters title and description of poll, redirects to poll/:id after storing poll in db
 router.post('/', (req, res) => {
   const newPoll = req.body;
   console.log(newPoll);
   return insertNewPoll(newPoll)
-  .then(poll => res.redirect(`/poll/${poll[0].id}`))
-  .catch(er => console.log('ERROR',er))
+    .then(poll => res.redirect(`/poll/${poll[0].id}`))
+    .catch(er => console.log('ERROR',er));
 });
+
+
 //Posts the entered options
 router.post('/:poll_id', (req, res) => {
   const options = req.body;
@@ -58,7 +54,7 @@ router.post('/:poll_id', (req, res) => {
     const op = `op${i}`;
     if (options[op]) {
       insertOptions(options[op], pollID)
-      .catch(e => console.log(e))
+        .catch(e => console.log(e));
     }
     if (!options[op]) {
     }
