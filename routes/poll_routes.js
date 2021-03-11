e1977c647548f58a59a764bfb1a3271feb525d1a
Samantha Knoop <sknoop@hotmail.ca>
2021-03-11 04:15:27 +0000

    fix git merge issues

--- routes/poll_routes.js
+++ routes/poll_routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const { insertNewPoll, updateURLs, insertOptions } = require('../db/queries/insert_new_poll.js');
+const { insertNewPoll,
+  updateURLs, insertOptions } = require('../db/queries/insert_new_poll.js')
 const { getUser } = require('../db/queries/user_queries.js');
-
 //endpoint to handle post requests to add basic poll info to database
 router.post('/:u_id', (req, res) => {
   if (!(req.session && req.session.user_id) || req.session.user_id !== Number(req.params.u_id)) {
@@ -11,7 +11,6 @@ router.post('/:u_id', (req, res) => {
   }
   const newPoll = {user_id: req.params.u_id, title: req.body.title, desc: req.body.desc, nums: req.body.nums};
   console.log(newPoll)
-
   return insertNewPoll(newPoll)
   .then(poll => {
     req.session.poll_id = poll[0].id;
@@ -19,11 +18,9 @@ router.post('/:u_id', (req, res) => {
     console.log(req.session.poll_id);
     console.log("xyzyx");
     res.redirect(`/poll/${poll[0].user_id}/${poll[0].id}`)
-
   })
   .catch(er => console.log('ERROR',er))
 });
-
 router.post('/:u_id/:poll_id', (req, res) => {
   if (!(req.session && req.session.user_id) || req.session.user_id !== Number(req.params.u_id) || req.session.poll_id !== Number(req.params.poll_id)) {
     res.status(403).send('<h3>You must be logged in and have a valid poll id.</h3>');
@@ -37,12 +34,9 @@ router.post('/:u_id/:poll_id', (req, res) => {
      .catch(e => console.log(ERROR, e));
   }
   res.redirect(`/admin/${req.params.u_id}/${req.params.poll_id}`);
-
 });
-
 //endpoint to handle GETs received at /:u_id. loads the create poll page
 router.get('/:u_id', (req, res) => {
-
   if (!(req.session && req.session.user_id) || req.session.user_id !== Number(req.params.u_id)) {
     res.status(403).send('<h3>You must be logged in .....</h3>');
     return;
@@ -56,10 +50,7 @@ router.get('/:u_id', (req, res) => {
     res.render('create_poll', templateVars);
   })
   .catch(er => console.log('ERROR',er));
-
 });
-
-
 //Renders admin_poll page where the user enters their options
 router.get('/:u_id/:poll_id', (req, res) => {
   let userObj;
@@ -68,21 +59,15 @@ router.get('/:u_id/:poll_id', (req, res) => {
     return;
   }
   return updateURLs(req.params)
-    .then((results) => {
-      console.log(results);
-      const templateVars = {poll_id: req.params.poll_id, opNum : results[0].numops};
-      res.render('admin_poll', templateVars);
-
+  .then((results) => {
     console.log(results);
     userObj = {email : req.session.email, id : req.session.user_id };
     const templateVars = {user: userObj, poll_id: req.params.poll_id, opNum : results[0].numops}
     res.render('admin_poll', templateVars)
-
+  })
+  .catch(er => console.log('ERROR',er))
 });
-
-
 //User enters title and description of poll, redirects to poll/:id after storing poll in db
-
 // //Posts the entered options
 // router.post('/:poll_id', (req, res) => {
 //   const options = req.body;
@@ -97,7 +82,6 @@ router.get('/:u_id/:poll_id', (req, res) => {
 //     if (!options[op]) {
 //     }
 //   }
-
 //   res.redirect(`/vote/${pollID}`);
 // });
 module.exports = router;
